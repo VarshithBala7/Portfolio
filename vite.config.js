5a8df7a1c14a995c73b90f2477b2356306a68756
VarshithBala7 <166023344+VarshithBala7@users.noreply.github.com>
2025-10-18 17:09:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default defineConfig({
+  plugins: [react()],
+  base: "/Portfolio/",   // must match your repo name exactly (case-sensitive)
+});
